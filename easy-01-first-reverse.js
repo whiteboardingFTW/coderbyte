f5d3b5aaf3e83e5af3067271d42682f5f583293e
Christopher Lee <yensen@gmail.com>
2016-02-22 19:24:37 -0800

    added first reverse solution

---
+++ easy-01-first-reverse.js
@@ -0,0 +1,12 @@
+// Have the function FirstReverse(str) take the str parameter being passed and return the string in reversed order.
+
+// Method #1
+function FirstReverse(str) {
+
+  var temp = [];
+  for (i = 0; i < str.length; i++) {
+    temp[i] = str[str.length-(i+1)];
+  }
+  return temp.join("");
+
+}
